769ac386381536215ae58734051a14d39c5f89c9
Luis Carrasco <luiscbilbao@gmail.com>
2023-02-28 19:25:05 -0600

    Fixing for Training scene

--- Assets/Scripts/Sensors/Audio/AudioSensorProxy.cs
+++ Assets/Scripts/Sensors/Audio/AudioSensorProxy.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.MLAgents.Sensors;
+using Unity.MLAgents;
 
 namespace MBaske.Sensors.Audio
 {
@@ -23,7 +24,7 @@ namespace MBaske.Sensors.Audio
 
         public CompressionSpec GetCompressionSpec()
         {
-            throw new System.NotImplementedException();
+            return new CompressionSpec(m_AudioSensor.CompressionType);
         }
 
         /// <inheritdoc/>
@@ -52,7 +53,12 @@ namespace MBaske.Sensors.Audio
 
         public ObservationSpec GetObservationSpec()
         {
-            throw new System.NotImplementedException();
+            InplaceArray<int> shape = new InplaceArray<int>(m_AudioSensor.Shape.Width, m_AudioSensor.Shape.Height, m_AudioSensor.Shape.Channels);
+            InplaceArray<DimensionProperty> prop = new InplaceArray<DimensionProperty>(DimensionProperty.Unspecified, DimensionProperty.Unspecified, DimensionProperty.Unspecified);
+            ObservationSpec test = new ObservationSpec(shape, prop);
+
+            //throw new NotImplementedException();
+            return test;
         }
 
         /// <inheritdoc/>
